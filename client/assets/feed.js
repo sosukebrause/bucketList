ebe0b62aa24225e261ff0593854e3836fd71d355
Sosuke Brause <brosuke89@gmail.com>
2020-07-13 13:03:52 -0700

    try large cards in feed.html

--- client/assets/feed.js
+++ client/assets/feed.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-  $.ajax({
-    type: "GET",
-    url: "/api/user_data",
-  }).then((user) => {
-    console.log(user);
-    $("#userId").text(user.id);
-  });
-
   getTodos().then((res) => {
     renderTodos(res);
   });
